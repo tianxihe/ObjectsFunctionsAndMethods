ad1cd67bb3347313a9f6e003cf29bae3edcf7d76
tianxihe <het@rose-hulman.edu>
2017-11-29 14:53:23 -0500

    2017-11-29 10:43

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -5,11 +5,11 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and Tianxi He.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ this comment, ASKING QUESTIONS as needed to understand it.
 #
 #   For objects that are CONSTRUCTED, we use the DOT notation
@@ -75,11 +75,14 @@ def main():
     #   First number:  bigger means faster.
     #   Second number: bigger means slower.
     window.tracer(1, 1)
-
+    try_methods_and_functions()
+    try_functions()
+    try_methods()
     jump_and_move_turtle(100, 50, 200, -100)
     turtle = rg.SimpleTurtle('square')
     draw_many_squares(turtle, 3, 75, 15)
     turtle3()
+
 
     # When the TODOs ask you to test YOUR code, put YOUR tests here:
 
@@ -162,6 +165,12 @@ def turtle3():
 
 
 def try_methods():
+    koto=rg.SimpleTurtle()
+    koto.pen=rg.Pen('brown', 5)
+    koto.forward(150)
+    koto.left(90)
+    koto.forward(50)
+    koto.backward(100)
     """
     Constructs a SimpleTurtle and sets its   pen   to a new rg.Pen
     that is 'brown' with thickness 5.
@@ -172,13 +181,17 @@ def try_methods():
       -- backward  100 units
     """
     ####################################################################
-    # TODO: 3. Implement this function, per its doc-string above.
+    # DONE: 3. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).
     ####################################################################
 
 
 def try_functions():
+    jump_and_move_turtle(200, 100, 300, 30)
+    jump_and_move_turtle(100, 200, 0, 0)
+    jump_and_move_turtle(-50, 50, 100, 100)
+
     """
     Causes several SimpleTurtles to do the following:
      -- One jumps to (200, 100), then moves (while drawing) to (300, 30)
@@ -186,12 +199,11 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ####################################################################
-    # TODO: 4. Implement this function, per its doc-string above.
+    # DONE: 4. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
     #    Keep reading the rest of this TO DO before doing the above!
     #
-    # IMPORTANT: This function requires
     #   ** exactly 3 lines **
     # If you think it needs more, ** ASK FOR HELP. **
     # HINT: see   jump_and_move_turtle   above.
@@ -200,6 +212,19 @@ def try_functions():
 
 
 def try_methods_and_functions():
+    koko=rg.SimpleTurtle()
+    koko.pen=rg.Pen('blue', 5)
+    koko.backward(150)
+    koko.speed=1
+    draw_many_squares(koko, 2, 100, 30)
+    koko.speed=5
+    koko.pen=rg.Pen('red',5)
+    draw_many_squares(koko, 10, 50, 15)
+    koko.speed=100
+    koko.pen=rg.Pen('red',35)
+    draw_many_squares(koko, 8, 300, 60)
+    koko.pen=rg.Pen('black',3)
+    koko.backward(200)
     """
     Constructs a SimpleTurtle and sets its   pen  to a new rg.Pen
     that is 'blue' with thickness 5.
@@ -232,7 +257,7 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ####################################################################
-    # TODO: 5. Implement this function, per its doc-string above.
+    # DONE: 5. Implement this function, per its doc-string above.
     #    Put a statement in   main   to test this function
     #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
     #    Keep reading the rest of this TO DO before doing the above!
